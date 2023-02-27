03146858d6e176c651900c5cc6dd4aa2dbb1cfd0
salgue441 <carlossalguero441@gmail.com>
2023-02-27 13:03:55 -0600

    Added two functioning routings

--- Rutas y Formas/app.js
+++ Rutas y Formas/app.js
@@ -1,34 +1,47 @@
-const http = require("http");
-const fs = require("fs");
+const http = require("http")
+const fs = require("fs")
 
 fs.readFile("./index.html", (err, html) => {
   if (err) {
-    throw err;
+    throw err
   }
+
   http
     .createServer((request, response) => {
       if (request.url === "/") {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
+        response.writeHeader(200, { "Content-Type": "text/html" })
+        response.write(html)
+        response.end()
       } else if (request.url === "/Michi") {
         fs.readFile("./Michi.html", (err, Michihtml) => {
-          response.writeHeader(200, { "Content-Type": "text/html" });
-          response.write(Michihtml);
-          response.end();
-        });
-      } else if (request.url === "/Datos") {
+          response.writeHeader(200, { "Content-Type": "text/html" })
+          response.write(Michihtml)
+          response.end()
+        })
+      } else if (request.url === "/Datos" && request.method === "POST") {
         fs.readFile("./Datos.html", (err, Datoshtml) => {
-          response.writeHeader(200, { "Content-Type": "text/html" });
-          response.write(Datoshtml);
-          response.end();
-        });
+          response.writeHeader(200, { "Content-Type": "text/html" })
+          response.write(Datoshtml)
+
+          let body = []
+          request
+            .on("data", (chunk) => {
+              body.push(chunk)
+            })
+
+            .on("end", () => {
+              body = Buffer.concat(body).toString()
+              console.log(body)
+            })
+
+          response.end()
+        })
       } else {
-        response.writeHeader(404);
-        response.write("<h1></h1>");
-        response.write("Not Found");
-        response.end();
+        response.writeHeader(404)
+        response.write("<h1></h1>")
+        response.write("Not Found")
+        response.end()
       }
     })
-    .listen(3000);
-});
+    .listen(3000)
+})
---
+++ Rutas y Formas/form.js
@@ -0,0 +1,5 @@
+// var form = document.getElementById(formID)
+
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault()
+// })
